f401ddaa0c0e15cda1b5863b3247b487dd92050d
artem <artemkag04@gmail.com>
2025-04-12 03:17:14 +0300

    Add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,15 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private final Map<String, Integer> nameToNumber = new HashMap<>();
+
     public int add(String name, int number) {
-        return 0;
+        if(!nameToNumber.containsKey(name)){
+            nameToNumber.put(name, number);
+        }
+        return nameToNumber.size();
     }
 }
